dabc09e1d6a05dff55777b48ada3173d7e999e0b
Werdy59999 <142753261+Werdy59999@users.noreply.github.com>
2026-03-17 11:21:49 +0100

    Update ServiceFoule.java

--- src/services/ServiceFoule.java
+++ src/services/ServiceFoule.java
@@ -1,5 +1,9 @@
 package services;
 
+import java.util.ArrayList;
+
+import models.Humain;
+
 /**
  * @version 1.0.0
  * @author Dylan Cardoso
@@ -7,7 +11,11 @@ package services;
  */
 public class ServiceFoule {
     
-    public static void genererFoule() {
-        
+    public static ArrayList<Humain> genererFoule(int taille) {
+        ArrayList<Humain> humains = new ArrayList<>();
+        for (int i = 0; i < taille; i++) {
+            humains.add(new Humain(( int ) ((Math.random() * (88 + 1)) - 16), "Humain [" + i + "]"));
+        }
+        return humains;
     }
 }
