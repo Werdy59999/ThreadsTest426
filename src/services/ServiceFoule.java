bfd1916edabeb1d72ce5d3c1e1eafd40601d9b9f
Werdy59999 <142753261+Werdy59999@users.noreply.github.com>
2026-03-17 11:05:14 +0100

    Create ServiceFoule.java

---
+++ src/services/ServiceFoule.java
@@ -0,0 +1,13 @@
+package services;
+
+/**
+ * @version 1.0.0
+ * @author Dylan Cardoso
+ * @since 17.03.2026
+ */
+public class ServiceFoule {
+    
+    public static void genererFoule() {
+        
+    }
+}
